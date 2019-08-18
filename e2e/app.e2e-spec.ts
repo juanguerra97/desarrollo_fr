7a3c56aad1ee773b381c24eb792ac26cc0833743
una mirada cientifica <f2kloa@gmail.com>
2019-08-18 16:07:03 -0600

    subiendo template

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,18 @@
 import { CoreUIPage } from './app.po';
 
+// @ts-ignore
 describe('core-ui App', function() {
   let page: CoreUIPage;
 
+  // @ts-ignore
   beforeEach(() => {
     page = new CoreUIPage();
   });
 
+  // @ts-ignore
   it('should display footer containing creativeLabs', () => {
     page.navigateTo();
+    // @ts-ignore
     expect(page.getParagraphText()).toContain('creativeLabs');
   });
 });
