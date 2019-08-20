279ab52ecbc87ec7bf3223550f73aada8aacbf6d
juanguerra97 <juanguerra97@protonmail.com>
2019-08-19 20:06:24 -0600

    Creacion del component Cursos

--- src/app/views/cursos/cursos.module.ts
+++ src/app/views/cursos/cursos.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CursosComponent } from './cursos/cursos.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CursosComponent],
   imports: [
     CommonModule
   ]
