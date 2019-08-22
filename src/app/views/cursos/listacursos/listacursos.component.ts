494473256cb4e114c999bf499648a285b04c53d5
Juan Guerra <jguerral4@miumg.edu.gt>
2019-08-22 17:02:49 -0600

    Se puede seleccionar un curso de la lista de cursos

--- src/app/views/cursos/listacursos/listacursos.component.ts
+++ src/app/views/cursos/listacursos/listacursos.component.ts
@@ -10,6 +10,9 @@ export class ListacursosComponent implements OnInit {
 
   @Input() cursos:ICurso[];
   @Output('ondeletecurso') ondelete = new EventEmitter<ICurso>();
+  @Output('onselectionchange') onselection = new EventEmitter<ICurso>();
+
+  private selected: ICurso = {codigo:-1,nombre:""};
 
   constructor() { }
 
@@ -19,6 +22,19 @@ export class ListacursosComponent implements OnInit {
 
   private onBorrarCurso(curso:ICurso){
     this.ondelete.emit(curso);
+  }
+
+  private onCursoClicked(curso:ICurso){
+    // no hay curso seleccionado o seleccion nueva
+    if(this.selected.codigo == -1 || this.selected.codigo != curso.codigo){
+      this.selected = curso;
+      this.onselection.emit(this.selected);
+    }else{// deseleccion del curso seleccionado anteriormente
+      this.selected = {codigo:-1,nombre:""};
+      this.onselection.emit(null);
+    }
+
+    
   }
 
 }
