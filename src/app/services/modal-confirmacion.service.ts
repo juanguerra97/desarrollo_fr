fe6028b2a0232bebdd6e6ddde5591241ba1ddbb3
Juan Guerra <jguerral4@miumg.edu.gt>
2019-08-22 18:22:05 -0600

    Component y Service para mostrar un modal de confirmacion de accion

---
+++ src/app/services/modal-confirmacion.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { ModalConfirmacionComponent } from '../views/modal-confirmacion/modal-confirmacion.component';
+
+@Injectable()
+export class ModalConfirmacionService {
+
+  constructor(
+    private modalService: NgbModal
+  ) { }
+
+  public mostrar(
+    titulo: string,
+    mensaje: string,
+    textoBtnConfirmar: string = 'Confirmar',
+    textoBtnCancelar: string = 'Cancelar',
+    dialogSize: 'sm'|'lg' = 'sm'): Promise<boolean> {
+      
+    const modalRef = this.modalService.open(ModalConfirmacionComponent, { 
+      size: dialogSize,
+      windowClass:"animated bounceIn" 
+    });
+
+    modalRef.componentInstance.titulo = titulo;
+    modalRef.componentInstance.mensaje = mensaje;
+    modalRef.componentInstance.textoBtnConfirmar = textoBtnConfirmar;
+    modalRef.componentInstance.textoBtnCancelar = textoBtnCancelar;
+
+    return modalRef.result;
+  }
+
+}
