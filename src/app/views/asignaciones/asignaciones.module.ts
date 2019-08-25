6d9fdeef4ceb867d2aec1ea6b4f883d6a8c3d482
Juan Guerra <jguerral4@miumg.edu.gt>
2019-08-25 15:32:56 -0600

    Cambios para comenzar la pantalla de asignaciones

--- src/app/views/asignaciones/asignaciones.module.ts
+++ src/app/views/asignaciones/asignaciones.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ListaComponent } from './lista/lista.component';
 import { NuevoComponent } from './nuevo/nuevo.component';
 import {AsignacionesRoutes} from './asignaciones.routes';
+import { AsignacionesComponent } from './asignaciones/asignaciones.component';
 
 
 
 @NgModule({
-  declarations: [ListaComponent, NuevoComponent],
+  declarations: [ListaComponent, NuevoComponent, AsignacionesComponent],
   imports: [
     CommonModule,
     AsignacionesRoutes
