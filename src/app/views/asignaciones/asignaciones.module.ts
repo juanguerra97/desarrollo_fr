26edc8b878c516a7137a0520ba728d6da199c976
Juan Guerra <jguerral4@miumg.edu.gt>
2019-08-25 17:10:50 -0600

    Componente para filtrar las asignaciones por seccion

--- src/app/views/asignaciones/asignaciones.module.ts
+++ src/app/views/asignaciones/asignaciones.module.ts
@@ -4,14 +4,17 @@ import { ListaComponent } from './lista/lista.component';
 import { NuevoComponent } from './nuevo/nuevo.component';
 import {AsignacionesRoutes} from './asignaciones.routes';
 import { AsignacionesComponent } from './asignaciones/asignaciones.component';
+import { FiltroAsigComponent } from './filtro-asig/filtro-asig.component';
+import { ReactiveFormsModule } from '@angular/forms'
 
 
 
 @NgModule({
-  declarations: [ListaComponent, NuevoComponent, AsignacionesComponent],
+  declarations: [ListaComponent, NuevoComponent, AsignacionesComponent, FiltroAsigComponent],
   imports: [
     CommonModule,
-    AsignacionesRoutes
+    AsignacionesRoutes,
+    ReactiveFormsModule
   ]
 })
 export class AsignacionesModule { }
