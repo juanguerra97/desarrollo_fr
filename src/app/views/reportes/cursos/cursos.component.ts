b55e8b64adc3046af9356b1a93d0217f0de5e0a7
HancelGH <54192092+HancelGH@users.noreply.github.com>
2019-09-04 10:29:12 -0600

    reporte 3 componente

--- src/app/views/reportes/cursos/cursos.component.ts
+++ src/app/views/reportes/cursos/cursos.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Reporte3Service } from '../../../services/reporte3.Service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.scss']
 })
-export class CursosComponent implements OnInit {
 
-  constructor() { }
+export class CursosComponent implements OnInit {
+  public reportes3 : any;
+
+constructor(private _cursorepService: Reporte3Service) {}
 
   ngOnInit() {
+    this._cursorepService.listReporte3().subscribe( res => { this.reportes3 = res[0]; },
+      err => console.error(err)
+    );
   }
-
-}
+};
