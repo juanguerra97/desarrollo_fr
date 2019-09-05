ce3be499afb52099333cd75b6d06dbde0a6fecc6
Juan Guerra <jguerral4@miumg.edu.gt>
2019-09-04 21:06:12 -0600

    Formulario de login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,7 +1,31 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent { }
+export class LoginComponent {
+
+  constructor(
+    private authService: AuthService
+  ){}
+
+  private formLogin = new FormGroup({
+    usuario: new FormControl('',[
+      Validators.required,
+      Validators.minLength(4)
+    ]),
+    contrasena: new FormControl('',[
+      Validators.required,
+      Validators.minLength(4)
+    ])
+  });
+
+
+  public onSubmit(){
+    this.authService.logIn(this.formLogin.value.usuario,this.formLogin.value.contrasena);
+  }
+
+}
