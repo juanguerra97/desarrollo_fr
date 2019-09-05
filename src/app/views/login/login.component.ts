d1eb91c2622e606c434c84e13d647e88c94b1d18
Juan Guerra <jguerral4@miumg.edu.gt>
2019-09-05 07:20:18 -0600

    Se muestran errores de login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -8,6 +8,8 @@ import {AuthService} from '../../auth/auth.service';
 })
 export class LoginComponent {
 
+  private error:string = null;
+
   constructor(
     private authService: AuthService
   ){}
@@ -25,7 +27,22 @@ export class LoginComponent {
 
 
   public onSubmit(){
-    this.authService.logIn(this.formLogin.value.usuario,this.formLogin.value.contrasena);
+    this.authService.logIn(this.formLogin.value.usuario,this.formLogin.value.contrasena)
+        .then((user:any)=>{
+          console.log("Bienvenido " + user.usuario)
+          this.error = null;
+        })
+      .catch((error:any)=>{
+        if(error.status == 0){
+          console.error(error);
+        } else{
+          this.error = error.error;
+        }
+      });
+  }
+
+  public onCloseErrorAlert(): void {
+    this.error = null;
   }
 
 }
