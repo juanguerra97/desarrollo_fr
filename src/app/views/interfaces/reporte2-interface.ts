670d5692a73ad2e65549573415974e9f8eda1ff8
Juan Guerra <jguerral4@miumg.edu.gt>
2019-09-05 11:15:20 -0600

    Otros cambios de Hancel

---
+++ src/app/views/interfaces/reporte2-interface.ts
@@ -0,0 +1,5 @@
+export interface reporte2Interface {
+  Codigo_catedratico: number;
+  anio: number;
+  no_semestre: number;
+}
--- src/app/views/reportes/catedraticos/catedraticos.component.ts
+++ src/app/views/reportes/catedraticos/catedraticos.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import {Reporte2Service} from '../../../services/reporte2.Service';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-catedraticos',
   templateUrl: './catedraticos.component.html',
   styleUrls: ['./catedraticos.component.scss']
 })
 export class CatedraticosComponent implements OnInit {
+  public conexiones;
 
-  constructor() { }
+  constructor(private reporte2Service: Reporte2Service,
+              private location: Location
+  ){
+  }
 
   ngOnInit() {
+
+  }
+  OnEnviar(reporteForm: NgForm ){
+    this.reporte2Service.buscarreporte2(reporteForm.value).subscribe((res)=> {this.conexiones = res, console.log(res)});
+
   }
 
 }
